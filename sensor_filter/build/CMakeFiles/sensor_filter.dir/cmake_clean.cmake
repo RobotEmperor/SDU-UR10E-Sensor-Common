27fdb9e5dd803cfd97b1b02a2383e17b254a485b
robotemperor <kimpd1224@gmail.com>
2020-02-04 16:26:35 +0100

    modified

---
+++ sensor_filter/build/CMakeFiles/sensor_filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensor_filter.dir/src/sensor_filter.cpp.o"
+  "libsensor_filter.pdb"
+  "libsensor_filter.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sensor_filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
